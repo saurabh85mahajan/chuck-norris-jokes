2f5349b52a733c5ba9dfac9db1b7ee28be1b05fc
Saurabh M <saurabh85mahajan@gmail.com>
2020-12-22 17:37:14 +0530

    Initial Commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Saurabh85mahajan\ChuckNorrisJokes;
+class JokeFactory
+{
+    protected $jokes = [
+        'Chuck Norris can strangle you with a cordless phone',
+        'Chuck Norris doesn\'t wear a watch. He decides what time it is.',
+        'Chuck Norris tells Simon what to do.',
+        'Chuck Norris makes onions cry.',
+    ];
+
+    public function __construct(array $jokes = null) 
+    {
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
+    }
+
+    public function getRandomJoke() 
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+}
---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Saurabh85mahajan\ChuckNorrisJokes\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Saurabh85mahajan\ChuckNorrisJokes\JokeFactory;
+
+class JokeFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_random_joke() 
+    {
+        $jokes = new JokeFactory([
+            'This is a joke',
+        ]);
+        $joke = $jokes->getRandomJoke();
+        
+        $this->assertSame('This is a joke', $joke);
+    }
+
+    /** @test */
+    public function it_returns_a_predefined_joke() 
+    {
+        $chuckNorrisjokes = [
+            'Chuck Norris can strangle you with a cordless phone',
+            'Chuck Norris doesn\'t wear a watch. He decides what time it is.',
+            'Chuck Norris tells Simon what to do.',
+            'Chuck Norris makes onions cry.',
+        ];
+        $jokes = new JokeFactory();
+        $joke = $jokes->getRandomJoke();
+        
+        $this->assertContains($joke, $chuckNorrisjokes);
+    }
+}
