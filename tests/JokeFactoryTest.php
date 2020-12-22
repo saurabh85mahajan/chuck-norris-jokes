6a2c0c5a63fb7be60af1fb2a77279c6625541679
Saurabh <saurabh85mahajan@users.noreply.github.com>
2020-12-22 17:09:44 +0000

    Apply fixes from StyleCI

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -4,9 +4,8 @@ namespace Saurabh85mahajan\ChuckNorrisJokes\Tests;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\HandlerStack;
-
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 use Saurabh85mahajan\ChuckNorrisJokes\JokeFactory;
 
@@ -18,7 +17,7 @@ class JokeFactoryTest extends TestCase
         $mock = new MockHandler([
             new Response(200, [], '{ "type": "success", "value": { "id": 464, "joke": "&quot;It works on my machine&quot; always holds true for Chuck Norris.", "categories": ["nerdy"] } }'),
         ]);
-        
+
         $handler = HandlerStack::create($mock);
         $client = new Client(['handler' => $handler]);
 
